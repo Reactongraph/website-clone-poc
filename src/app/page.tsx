7a535247e8d79e789150189a796cd18df9892cf8
Reactongraph <js.developer@ongraph.com>
2024-01-04 18:50:25 -0500

    made homepage, img-SliderPage for desktop screen and revoulte cardPage (WIP)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
 "use client";
-
 import Benefits from "./components/benefits/page";
 import Footer from "./components/footer/page";
 import GetStarted from "./components/getStarted/page";
 import TrustedBy from "./components/trustedBy/page";
 import { MainContainerWapper } from "./styled";
+import Footer2 from "./components/footer2/page";
+import SlidingCard from "./components/slidingCard/page";
+import HomePage from "./components/homePage/page";
+import RevoluteCards from "./components/revoluteCards/page";
+
 
 export default function Home() {
   return (
-    <MainContainerWapper>
-      <GetStarted/>
-      <TrustedBy/>
-      <Benefits/>
-      <Footer/>
-    </MainContainerWapper>
+    // <div>
+    // <MainContainerWapper>
+    //   <GetStarted/>
+    //   <TrustedBy/>
+    //   <Benefits/>
+    //   <Footer/>
+    // </MainContainerWapper>
+    // </div>
+    
+    <div>
+      <HomePage/>
+      <SlidingCard/>
+      <RevoluteCards/>
+      {/* <Footer2/> */}
+    </div>
   );
 }
